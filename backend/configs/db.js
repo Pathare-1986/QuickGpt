f18a46706abbd67c0b6e9ea8174138b65abe26a3
Tushar Pathare <patharetushar10@gmail.com>
2025-09-14 22:52:36 +0530

    end backend

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async ()=>{
     try {
         mongoose.connection.on("connected",()=>console.log("DataBase connected"))
-        await mongoose.connect(`${process.env.MONGODB_URI}/quickGpt`)
+        await mongoose.connect(`${process.env.MONGODB_URI}`)
         
     } catch (error) {
         console.log(error.message)
